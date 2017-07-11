5453b60e8394a5d0764b6705cfd196b2f3c7ff18
Alexander Grafov <grafov@gmail.com>
2017-07-11 22:42:28 +0300

    Redefine mouse properties

---
+++ keyboards/ergodox/keymaps/grafov/config.h
@@ -0,0 +1,36 @@
+#ifndef CONFIG_GRAFOV_H
+#define CONFIG_GRAFOV_H
+
+#include "../../config.h"
+
+#undef MOUSEKEY_DELAY
+#undef MOUSEKEY_INTERVAL
+#undef MOUSEKEY_MAX_SPEED
+#undef MOUSEKEY_TIME_TO_MAX
+#undef MOUSEKEY_WHEEL_MAX_SPEED
+#undef MOUSEKEY_WHEEL_TIME_TO_MAX
+#undef MOUSEKEY_WHEEL_DELAY
+
+#define MOUSEKEY_DELAY             200 // 300
+#define MOUSEKEY_INTERVAL          50
+#define MOUSEKEY_MAX_SPEED         15 // 10
+#define MOUSEKEY_TIME_TO_MAX       5  // 20
+#define MOUSEKEY_WHEEL_MAX_SPEED   9  // 8
+#define MOUSEKEY_WHEEL_TIME_TO_MAX 20 // 40
+#define MOUSEKEY_WHEEL_DELAY       1
+
+//#define ONESHOT_TAP_TOGGLE 2
+//#define ONESHOT_TIMEOUT 3000
+
+//#undef LOCKING_SUPPORT_ENABLE
+//#undef LOCKING_RESYNC_ENABLE
+
+//#undef LEADER_TIMEOUT
+//#define LEADER_TIMEOUT 1000
+
+//#define LSPO_KEY KC_A
+//#define RSPC_KEY TO(2)
+
+#endif
+
+
--- keyboards/ergodox/keymaps/grafov/keymap.c
+++ keyboards/ergodox/keymaps/grafov/keymap.c
@@ -9,12 +9,6 @@
 
 #define _____ KC_TRANSPARENT
 
-/* #define MOUSEKEY_DELAY             200 // 300 */
-/* #define MOUSEKEY_INTERVAL          50 */
-/* #define MOUSEKEY_MAX_SPEED         15 // 10 */
-/* #define MOUSEKEY_TIME_TO_MAX       20 */
-/* #define MOUSEKEY_WHEEL_MAX_SPEED   9  // 8 */
-/* #define MOUSEKEY_WHEEL_TIME_TO_MAX 20 // 40 */
 
 // Layer names
 enum {
