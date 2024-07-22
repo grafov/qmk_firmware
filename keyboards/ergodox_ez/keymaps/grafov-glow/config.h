648a84a80febea0bb22bf1d6e6bf978bad302ee3
Alexander I.Grafov <grafov@inet.name>
2024-07-22 11:27:22 +0300

    Remove unused funcs

--- keyboards/ergodox_ez/keymaps/grafov-glow/config.h
+++ keyboards/ergodox_ez/keymaps/grafov-glow/config.h
@@ -172,15 +172,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define ENABLE_RGB_MATRIX_SOLID_SPLASH
 #define ENABLE_RGB_MATRIX_SOLID_MULTISPLASH
 
-// #define RGBLIGHT_COLOR_LAYER_0 0x00, 0x00, 0xFF
-/* #define RGBLIGHT_COLOR_LAYER_1 0x00, 0x00, 0xFF */
-/* #define RGBLIGHT_COLOR_LAYER_2 0xFF, 0x00, 0x00 */
-/* #define RGBLIGHT_COLOR_LAYER_3 0x00, 0xFF, 0x00 */
-/* #define RGBLIGHT_COLOR_LAYER_4 0xFF, 0xFF, 0x00 */
-/* #define RGBLIGHT_COLOR_LAYER_5 0x00, 0xFF, 0xFF */
-/* #define RGBLIGHT_COLOR_LAYER_6 0xFF, 0x00, 0xFF */
-/* #define RGBLIGHT_COLOR_LAYER_7 0xFF, 0xFF, 0xFF */
-
 /*
  * Feature disable options
  *  These options are also useful to firmware size reduction.
