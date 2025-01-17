5c044c5240690184a5fb6ec926551874c499f003
Alexander I.Grafov <grafov@inet.name>
2025-01-18 00:45:11 +0300

    Update layout switching keys
    Updated the layout switching keys to use KC_LNG3 and KC_LNG4, which
    correspond to foreign and native language switch codes in evdev.

--- keyboards/ergodox_ez/keymaps/grafov-glow/keymap.h
+++ keyboards/ergodox_ez/keymaps/grafov-glow/keymap.h
@@ -3,11 +3,17 @@
 #define XXXX KC_NO
 
 // Switchers
-//#define RUS      KC_SCROLLLOCK
-//#define LAT      KC_SCROLLLOCK
+// #define RUS      KC_SCROLLLOCK
+// #define LAT      KC_SCROLLLOCK
 // return to shift-shift utility for layout switching
-#define RUS      KC_RSFT
-#define LAT      KC_LSFT
+// #define RUS      KC_RSFT
+// #define LAT      KC_LSFT
 
+// evdev uses https://raw.githubusercontent.com/torvalds/linux/v6.12/include/uapi/linux/input-event-codes.h
+// foreign as KC_KATAKANA code 90
+#define LAT KC_LNG3
+
+// native as KC_HIRAGANA code 91
+#define RUS KC_LNG4
 
 void switch_russian_layer(bool back);
